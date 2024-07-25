dd3ac8ed9f174b7ad87f288fecfb4828cc99fc0b
farah mourad <farahmourad027@gmail.com>
2024-07-25 18:36:50 +0300

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Endpoint 1: Home
+app.get('/', (req, res) => {
+    res.send('Hello, this is the home endpoint!');
+});
+
+// Endpoint 2: About
+app.get('/about', (req, res) => {
+    res.send('Hello, this is the about endpoint!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
